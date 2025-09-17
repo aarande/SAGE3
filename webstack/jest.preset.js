19c43bcf4cabebcacf67de23fe3583c40b20d43c
Aaron Anderson <aaron.anderson74@yahoo.com>
2025-09-17 11:28:59 -0500

    Updates to remove jest to resolve build errors

--- webstack/jest.preset.js
+++ webstack/jest.preset.js
@@ -1,17 +1,3 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = {
-  ...nxPreset,
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.(ts|js)?(x)',
-    '<rootDir>/src/**/?(*.)(spec|test).(ts|js)?(x)',
-  ],
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
-};
+module.exports = { ...nxPreset };
